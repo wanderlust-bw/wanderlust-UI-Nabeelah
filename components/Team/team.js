04d6f26917596e531495dff8bfe855fb384d21f7
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-23 12:16:11 +0100

    Add animation to team page

---
+++ components/Team/team.js
@@ -0,0 +1,18 @@
+class Team {
+    constructor(items) {
+        this.items = items;
+        window.addEventListener('load', () => this.animate())
+    }
+    animate() {
+        let timeline = new TimelineMax();
+        this.items.forEach((item) => {
+            timeline.from(item, .5, { opacity: 0 })
+        })
+    }
+}
+
+let teamCards = document.querySelectorAll('.team-card');
+
+new Team(teamCards);
+
+console.log(teamCards);
